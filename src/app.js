b0cfac91140f1b8da04a6ee18b150d66c342583a
mamba666 <lwjkkbb@sina.com>
2020-05-11 11:08:13 +0800

    refactor:调整目录结构

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+/**
+ * 业务
+ */
+
 const Koa = require('koa')
 const app = new Koa()
 const views = require('koa-views')
@@ -6,6 +10,7 @@ const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
+// 从routes文件夹引入路由
 const index = require('./routes/index')
 const users = require('./routes/users')
 
@@ -14,31 +19,37 @@ onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
+// 将public文件夹注册为静态化
+// 可以通过这个地址访问静态资源
+// http://localhost:3000/stylesheets/style.css
 app.use(require('koa-static')(__dirname + '/public'))
 
+// 注册ejs
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }))
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// 中间件的演示,和上面的logger()是一样的
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
 // routes
+// 注册路由
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
-});
+})
 
 module.exports = app
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('koa-router')()
 
+// 前缀，即下面所有的路由前面都有 /users 前缀
 router.prefix('/users')
 
 router.get('/', function (ctx, next) {
