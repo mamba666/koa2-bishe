5b5bf87ab4383e0f2685e949a20dfe008a76979f
mamba666 <lwjkkbb@sina.com>
2020-05-11 14:28:15 +0800

    feat:路由学习

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,33 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
+// 返回json格式
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+// 个人主页
+// 需要使用到动态参数
+router.get('/profile/:username', async (ctx, next) => {
+  const { username } = ctx.params
+  console.log(ctx.params)
+  ctx.body = {
+    title: 'koa2 json',
+    username,
+    ctx
+  }
+})
+
+// 加载更多
+// 比如说加载到第几页,就要显示第几页
+// 这里也是动态参数,但是动态参数并不是一个,获取方法和上面也是一样,ctx.params
+router.get('/loadMore/:username/:pageIndex', async (ctx, next) => {
+  const { username, pageIndex } = ctx.params
+  ctx.body = {
+    username,
+    pageIndex
   }
 })
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -11,4 +11,13 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+// post
+router.post('/login', async (ctx, next) => {
+  const { username, password } = ctx.request.body
+  ctx.body = {
+    username,
+    password
+  }
+})
+
 module.exports = router
